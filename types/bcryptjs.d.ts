26548f44a3810fc003936988274d0647568a9005
jefrayn <salinasj.jr@gmail.com>
2025-04-30 18:24:40 -0400

    Fix: add custom declaration for bcryptjs

--- types/bcryptjs.d.ts
+++ types/bcryptjs.d.ts
@@ -1,1 +1,5 @@
-declare module 'bcryptjs';
+declare module 'bcryptjs' {
+    export function hash(password: string, saltRounds: number): Promise<string>;
+    export function compare(password: string, hash: string): Promise<boolean>;
+  }
+  
